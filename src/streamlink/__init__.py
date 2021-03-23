3f93da033e9517ebdae7ce1c5a97a1a3b99fd172
Michael Hsin <yhhsin@gmail.com>
2021-03-23 03:15:35 +0000

    Fix eventlet.monkey_patch().

--- src/streamlink/__init__.py
+++ src/streamlink/__init__.py
@@ -20,7 +20,7 @@ __credits__ = ["https://github.com/streamlink/streamlink/blob/master/AUTHORS"]
 
 import eventlet
 # Must be as early as possible
-eventlet.monkey_patch(socket=True, thread=True)
+eventlet.monkey_patch(socket=True, thread=True, os=True, select=True, time=True)
 
 from streamlink.api import streams
 from streamlink.exceptions import (StreamlinkError, PluginError, NoStreamsError,
